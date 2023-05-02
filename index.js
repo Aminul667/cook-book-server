f11a6cdeb59b245e1f268cce47fcd1eca7e55659
Aminul667 <rahathssn@gmail.com>
2023-05-02 16:10:59 +0600

    added chefs data

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Chef server is running');
 });
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
 app.listen(port, () => {
     console.log(`Chef API is running on the port: ${port}`);
